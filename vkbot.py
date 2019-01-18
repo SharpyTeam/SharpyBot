78c9626c7b3fcfd5b6e5056a88c033c2a3b6a146
Alexander <selya.main@gmail.com>
2019-01-18 03:41:11 +0300

    Git merging fixes.

--- vkbot.py
+++ vkbot.py
@@ -1,4 +1,5 @@
 import random
+import subprocess
 
 import vk
 from flask import Flask, request, json
@@ -29,7 +30,9 @@ def processing():
 @app.route('/repo_push', methods = ['POST'])
 def repo_push():
     # hardcoded, but i do not give a shit
-    subprocess.run(["git", "pull"], cwd = '/home/selya/vkbot')
+    subprocess.run(["git", "fetch", "--all"], cwd = '/home/selya/vkbot')
+    subprocess.run(["git", "reset", "--hard", "origin/master"], cwd = '/home/selya/vkbot')
+    subprocess.run(["git", "pull", "origin", "master"], cwd = '/home/selya/vkbot')
     subprocess.run(["touch", "/var/www/selya_pythonanywhere_com_wsgi.py"])
     return 'ok'
     
