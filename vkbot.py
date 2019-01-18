6c000622115afa90ed432ec28b293fd7b896924f
Alexander <selya.main@gmail.com>
2019-01-18 04:13:48 +0300

    Fixed кошкодевочка v2.

--- vkbot.py
+++ vkbot.py
@@ -32,7 +32,6 @@ def repo_push():
     # hardcoded, but i do not give a shit
     subprocess.run(["git", "fetch", "--all"], cwd = '/home/selya/vkbot')
     subprocess.run(["git", "reset", "--hard", "origin/master"], cwd = '/home/selya/vkbot')
-    subprocess.run(["git", "pull", "origin", "master"], cwd = '/home/selya/vkbot')
     subprocess.run(["touch", "/var/www/selya_pythonanywhere_com_wsgi.py"])
     return 'ok'
     
